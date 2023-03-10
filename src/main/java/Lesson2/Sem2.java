4acd4b1e3ac345b9619aea7f07618306dd4e1378
Monte_Kristo_VM <monte_kristo2003@mail.ru>
2023-03-10 15:30:58 +0300

    Task 2 completed

--- src/main/java/Lesson2/Sem2.java
+++ src/main/java/Lesson2/Sem2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Sem2 {
     public static void main(String[] args) {
         System.out.print(build());
+        System.out.print(letterCount());
     }
 
     /*Задание №1
@@ -25,5 +26,34 @@ public class Sem2 {
             sb.append((i % 2 == 0) ? first : second);
         }
         return sb;
+    }
+
+
+    /* Задание №2
+        Напишите метод, который сжимает строку.
+        Пример: вход aaaabbbcdd.
+    */
+    public static StringBuilder letterCount(){
+        StringBuilder sb = new StringBuilder();
+        String str = "aaaabbbcddeeef";
+        int count = 1;
+        for (int i = 0; i < str.length()- 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)){
+                count++;
+            }else {
+                if ((count == 1)) {
+                    sb.append(str.charAt(i));
+                } else {
+                    sb.append(str.charAt(i)).append(count);
+                }
+                count = 1;
+            }
+        }
+        if ((count == 1)) {
+            sb.append(str.charAt(str.length()-1));
+        } else {
+            sb.append(str.charAt(str.length()-1)).append(count);
+        }
+        return  sb;
     }
 }
